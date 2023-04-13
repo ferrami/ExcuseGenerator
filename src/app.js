30f44241bb23269da036f726293fd3bb4200ea7f
Fernando Ramirez <86069304+ferrami@users.noreply.github.com>
2023-04-13 01:34:06 +0000

    Completed v1

--- src/app.js
+++ src/app.js
@@ -1,11 +1,56 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./assets/img/excuse.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#excuses").innerHTML = GenerateExcuse();
+  });
 };
+
+function GenerateExcuse() {
+  var pronoun = ["A", "The", "My"];
+  var subject = [
+    " dog",
+    " superhero",
+    " ninja",
+    " cat",
+    " baby",
+    " Pink Elephant"
+  ];
+  var action = [
+    "kicked",
+    "ate",
+    "kicked",
+    "snapped",
+    "minced",
+    "burned",
+    "escaped with",
+    "bite",
+    "stole"
+  ];
+  var object = [
+    " my homework",
+    " my project",
+    " my car",
+    " my shoe",
+    " my report",
+    " my alarm clock",
+    " my sister"
+  ];
+  var place = [
+    " in my house",
+    " on the street",
+    " in my way to here",
+    " in the store",
+    " in my basement"
+  ];
+
+  var pron = pronoun[Math.round(Math.random() * (pronoun.length - 1))];
+  var who = subject[Math.round(Math.random() * (subject.length - 1))];
+  var did = action[Math.round(Math.random() * (action.length - 1))];
+  var what = object[Math.round(Math.random() * (object.length - 1))];
+  var where = place[Math.round(Math.random() * (place.length - 1))];
+
+  return pron + "" + who + " " + did + "" + what + "" + where;
+}
